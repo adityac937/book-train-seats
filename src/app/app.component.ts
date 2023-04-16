82ec82ea55fed077861358e7f4c46a5289e544b9
Aditya Chaudhari <aditya.ch@keka.com>
2023-04-16 13:05:32 +0530

    Book seats in same row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'traveller';
+export class AppComponent implements OnInit {
+  numberOfSeatsToBook: number;
+  totalSeats: Array<SeatType> = [];
+  availableSeatsInThatRow: Array<SeatType> = [];
+  totalSeatsAvailable: number;
+  showError: boolean;
+  showSeatUnavailableError: boolean;
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 80; i++) { this.totalSeats.push({ seatNumber: i, isSelected: false }) }
+  }
+
+  allotSeats() {
+    let maxSeatsAllowedInRow = 7
+    let numberOfCompleteRows = (this.totalSeats.length / maxSeatsAllowedInRow).toFixed()
+    let lastRowSeatsAvailable = this.totalSeats.slice(-3).filter((i) => i.isSelected == false);
+    this.totalSeatsAvailable = this.totalSeats.filter((i) => i.isSelected === false).length
+    if (this.numberOfSeatsToBook > maxSeatsAllowedInRow) {
+      this.showError = true;
+    }
+
+    if (this.totalSeatsAvailable < this.numberOfSeatsToBook) {
+      this.showSeatUnavailableError = true;
+    }
+
+    else if (this.numberOfSeatsToBook <= maxSeatsAllowedInRow) {
+      this.showError = false;
+      this.showSeatUnavailableError = false;
+      let rowNumber: number;
+      for (let i = 1; i <= +numberOfCompleteRows; i++) {
+        rowNumber = i;
+        this.availableSeatsInThatRow = [];
+        for (let j = ((rowNumber * maxSeatsAllowedInRow) - maxSeatsAllowedInRow); j < rowNumber * maxSeatsAllowedInRow; j++) {
+          if (this.totalSeats[j].isSelected === false) { this.availableSeatsInThatRow.push(this.totalSeats[j]) }
+          if (this.availableSeatsInThatRow.length == this.numberOfSeatsToBook) {
+            break;
+          }
+        }
+        if (this.availableSeatsInThatRow.length == this.numberOfSeatsToBook) {
+          break;
+        }
+      }
+
+      if (this.numberOfSeatsToBook <= lastRowSeatsAvailable.length) {
+        this.totalSeats.forEach((i) => {
+          for (let j = 0; j < this.numberOfSeatsToBook; j++) {
+            if (i.seatNumber === lastRowSeatsAvailable[j].seatNumber) { i.isSelected = true }
+          }
+        })
+      } else {
+        if (this.availableSeatsInThatRow.length == this.numberOfSeatsToBook) {
+          this.totalSeats.forEach((i) => {
+            this.availableSeatsInThatRow.forEach(j => {
+              if (j.seatNumber === i.seatNumber) {
+                i.isSelected = true
+              }
+            })
+          })
+        } else {
+          this.totalSeats.filter((i) => i.isSelected === false).slice(0, this.numberOfSeatsToBook).forEach((i) => i.isSelected = true)
+        }
+      }
+    }
+  }
+}
+
+interface SeatType {
+  seatNumber: number;
+  isSelected: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
